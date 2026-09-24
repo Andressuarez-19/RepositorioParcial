516265d02d6698e866567ff46dad55d404f10e21
Andressuarez-19 <andresf.suarezc@uqvirtual.edu.co>
2026-09-24 17:50:44 -0500

    agrego getter y setter

--- Parcial/src/main/java/model/Habitacion.java
+++ Parcial/src/main/java/model/Habitacion.java
@@ -1,4 +1,66 @@
 package model;
 
 public class Habitacion {
+
+
+    // GETTERS Y SETTERS
+    public int getNumeroHabitacion() {
+        return numeroHabitacion;
+    }
+
+    public void setNumeroHabitacion(int numeroHabitacion) {
+        this.numeroHabitacion = numeroHabitacion;
+    }
+
+    public int getPiso() {
+        return piso;
+    }
+
+    public void setPiso(int piso) {
+        this.piso = piso;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public double getPrecioPorNoche() {
+        return precioPorNoche;
+    }
+
+    public void setPrecioPorNoche(double precioPorNoche) {
+        this.precioPorNoche = precioPorNoche;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitacion{" +
+                "numeroHabitacion=" + numeroHabitacion +
+                ", piso=" + piso +
+                ", tipo='" + tipo + '\'' +
+                ", capacidadMaxima=" + capacidadMaxima +
+                ", precioPorNoche=" + precioPorNoche +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
 }
