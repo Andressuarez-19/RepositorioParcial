d8a93f3693443e099f2f10d1254f147b2a910c53
Tapasco06 <juanjoseft06@gmail.com>
2026-09-24 17:20:59 -0500

    atributos de la clase reserva creados

---
+++ Parcial/src/main/java/co/edu/uniquindio/model/Reserva.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Reserva {
+    // Atributos de la clase Reserva
+    private String codigoReserva;
+    private Date fechaRealizacion;
+    private Date fechaEntrada;
+    private Date fechaSalida;
+    private String estado;
+    private String metodoPago;
+    private double valorTotal;
+}
