aab02169c6de5f67f9f3ff5eabf36b72ad84e89f
Tapasco06 <juanjoseft06@gmail.com>
2026-09-24 17:14:36 -0500

    atributos de huesped creados

--- Parcial/src/main/java/co/edu/uniquindio/model/Huesped.java
+++ Parcial/src/main/java/co/edu/uniquindio/model/Huesped.java
@@ -1,4 +1,11 @@
 package co.edu.uniquindio.model;
 
 public class Huesped {
+    // Atributos de la clase Huesped
+    private String nombreCompleto;
+    private String documentoIdentidad;
+    private String telefono;
+    private String correoElectronico;
+    private String paisProcedencia;
+
 }
