1c934a67a54dcf17243011603a90cf9e22f9d0dd
Tapasco06 <juanjoseft06@gmail.com>
2026-09-24 17:22:18 -0500

    Metodo constructor de la clase Reserva creado

--- Parcial/src/main/java/co/edu/uniquindio/model/Reserva.java
+++ Parcial/src/main/java/co/edu/uniquindio/model/Reserva.java
@@ -13,4 +13,29 @@ public class Reserva {
     private String estado;
     private String metodoPago;
     private double valorTotal;
+
+    // Relaciones de la clase Reserva
+    private Huesped huesped;
+    private List<Habitacion> listReservaHabitaciones;
+    private List<ServicioAdicional> listReservaServicios;
+
+    /**
+     * Metodo constructor de la clase Reserva
+     */
+    public Reserva(String codigoReserva,
+                   Date fechaRealizacion,
+                   Date fechaEntrada,
+                   Date fechaSalida,
+                   String estado,
+                   String metodoPago,
+                   Huesped huesped) {
+
+        this.codigoReserva = codigoReserva;
+        this.fechaRealizacion = fechaRealizacion;
+        this.fechaEntrada = fechaEntrada;
+        this.fechaSalida = fechaSalida;
+        this.estado = estado;
+        this.metodoPago = metodoPago;
+        this.huesped = huesped;
+        this.valorTotal = 0;
 }
