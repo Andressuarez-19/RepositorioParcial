0bb0e578a04bebbb1907b197a015f9981c212718
Tapasco06 <juanjoseft06@gmail.com>
2026-09-24 17:15:16 -0500

    Constructor de la clase Huesped creado

--- Parcial/src/main/java/co/edu/uniquindio/model/Huesped.java
+++ Parcial/src/main/java/co/edu/uniquindio/model/Huesped.java
@@ -8,4 +8,20 @@ public class Huesped {
     private String correoElectronico;
     private String paisProcedencia;
 
+    /**
+     * Constructor de la clase Huesped
+     */
+    public Huesped(String nombreCompleto,
+                   String documentoIdentidad,
+                   String telefono,
+                   String correoElectronico,
+                   String paisProcedencia) {
+
+        this.nombreCompleto = nombreCompleto;
+        this.documentoIdentidad = documentoIdentidad;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+        this.paisProcedencia = paisProcedencia;
+    }
+
 }
