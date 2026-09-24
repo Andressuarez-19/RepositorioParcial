272e1959b60141b57dc62a2ddef29c8dfe30c42c
Tapasco06 <juanjoseft06@gmail.com>
2026-09-24 17:50:32 -0500

    habitaciones creadas

--- Parcial/src/main/java/co/edu/uniquindio/Main.java
+++ Parcial/src/main/java/co/edu/uniquindio/Main.java
@@ -11,4 +11,53 @@ public class Main {
     public static void main(String[] args) {
 
         Hotel hotel = new Hotel("StayPlus", "900123456", "Armenia", "3111234567", "www.stayplus.com");
-}
+        //==========================================
+        // HABITACIONES
+        // ==========================================
+
+        hotel.registrarHabitacion(
+                101,
+                1,
+                "Individual",
+                1,
+                80000,
+                "Disponible"
+        );
+
+        hotel.registrarHabitacion(
+                102,
+                1,
+                "Individual",
+                1,
+                80000,
+                "Disponible"
+        );
+
+        hotel.registrarHabitacion(
+                201,
+                2,
+                "Doble",
+                2,
+                120000,
+                "Disponible"
+        );
+
+        hotel.registrarHabitacion(
+                202,
+                2,
+                "Doble",
+                2,
+                120000,
+                "Disponible"
+        );
+
+        hotel.registrarHabitacion(
+                301,
+                3,
+                "Suite",
+                4,
+                200000,
+                "Disponible"
+        );
+
+    }
